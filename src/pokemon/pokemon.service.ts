72e83cf3001a462d53fc77179dc44b1e755b5129
Marco Vega Rojas <marco.vega@slepelqui.gob.cl>
2026-08-16 23:20:54 -0400

    update

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,6 +1,6 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -31,15 +31,37 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let poke: Pokemon | null = null;
+    //busca por numero
+    if (!isNaN(+term)) {
+      poke = await this.pokemonModel.findOne({ numero: +term }).exec();
+    }
+
+
+    //mongoID
+    //busca por id mongo ID
+    if (!poke &&isValidObjectId(term)) { //si no encuentra el pokemon y es un id valido de mongo
+      poke = await this.pokemonModel.findById(term);
+    }
+
+    //Busca por nombre
+    if (!poke) {
+      poke = await this.pokemonModel.findOne({ nombre: term.toLowerCase().trim() });
+    }
+
+    if (!poke) 
+      throw new NotFoundException(`No se ha encontrado Pokemon por  "${term}" not found`);
+
+    return poke;
+
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    return `This action updates a #${term} pokemon`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  remove(term: string) {
+    return `This action removes a #${term} pokemon`;
   }
 }
